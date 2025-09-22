43669cd27be8a2ef37b9dd2beaea3bc3f1b6daa6
zumer305 <zumerniaz305@gmail.com>
2025-09-23 01:57:45 +0500

    update

--- index.js
+++ index.js
@@ -15,9 +15,44 @@ const app=express();
 
 
 
+
+
+
+// req send !!!!!!!!!!!!!
 let port=3000;
 app.listen(port,()=>{
     console.log(`app is listening on port ${port}`);
 
 })  //server starts listening------to stop (ctrl+c)
 // localhost:3000  ------on browser(make sure server start krky )===node index.js
+
+
+
+
+
+// req recieve  !!!!!!!!!!!  at a time 1 path pr 1 he response
+app.get("/",(req,res)=>{     //app.use just show localhost:3000
+    console.log("req recieved");
+
+//  on localhost:3000 
+    res.send("This is a basic response");  
+    // res.send({name:"apple",fruit:"yes"});
+    // let code="<h1>Animals<h1>";
+    // res.send(code); 
+
+})
+
+app.get("/apple",(req,res)=>{
+    res.send("This is apple page");
+})
+
+app.get("/orange",(req,res)=>{
+    res.send("This is orange page");
+})
+// Put this at the very bottom of your routes
+app.use((req, res) => {
+  res.status(404).send("This page does not exist");
+});
+
+
+
